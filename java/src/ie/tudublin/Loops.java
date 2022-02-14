ab19d4a51977d32814e2fba8c6a1593e9c25ee14
iExclusive <C20321911@mytudublin.ie>
2022-02-14 12:54:12 +0000

    circles

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -2,41 +2,76 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Loops extends PApplet
-{
-
+public class Loops extends PApplet {
 
 	int mode = 0;
 
-	public void settings()
-	{
+	public void settings() {
 		size(500, 500);
 	}
 
 	public void setup() {
 		colorMode(HSB);
-		
+
 	}
 
-	public void keyPressed()
-	{
-		if (key >= '0' && key <='9')
-		{
+	public void keyPressed() {
+		if (key >= '0' && key <= '9') {
 			mode = key - '0';
 		}
 		println(mode);
 	}
 
-	
-	public void draw()
-	{
-		
-		switch(mode)	
-		{
+	float offset;
+
+	public void draw() {
+
+		switch (mode) {
 			case 0:
+				background(0);
+				int bars = (int) (mouseX / 20.0f);
+				float w = width / (float) bars;
+				for (int i = 0; i < bars; i++) {
+					noStroke();
+					fill(map(i, 0, bars, 0, 255), 255, 255);
+					rect(map(i, 0, bars, 0, 500), 0, w, height);
+				}
 				break;
-			case 1:
+			case 1: {
+				background(0);
+				int squares = (int) (mouseX / 20.0f);
+				float h = width / (float) squares;
+				for (int i = 0; i < squares; i++) {
+					noStroke();
+					fill(map(i, 0, squares, 0, 255), 255, 255);
+					float x = map(i, 0, squares, 0, width);
+					rect(x, x, h, h);
+					rect((width - h) - x, x, h, h);
+				}
+				// map(a, b, c, d, e);
+				// a = inputvalue
+				// b-c - start and end of the first range
+				// d, e 0=start and end of the end range
+
+				// map(-2, 10, 90, 200, 233);
+
 				break;
+			}
+			case 2:
+			background(0);
+			int circles2 = (int) (mouseX / 20.0f);
+			//offset += (mouseY / 100.0f);
+			float diamater = width / (float) circles2;
+			for (int j = 0; j < circles2; j++) {
+				for (int i = 0; i < circles2; i++) {
+					noStroke();
+					float c = map((i+j+offset), 0, circles2 * 2, 0, 255) % 256;
+					fill(c, 255, 255);
+					float x = map(i, 0, circles2 - 1, diamater / 2.0f, width - (diamater / 2.0f));
+					float y = map(j, 0, circles2 - 1, diamater / 2.0f, width - (diamater / 2.0f));
+					circle(x, y, diamater);
+				}
+			}
 		}
 	}
 }
